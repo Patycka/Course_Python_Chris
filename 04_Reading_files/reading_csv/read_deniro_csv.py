7aed6bb0d5b7f1a6d3c06cdf3c1b1395ed0ceeed
Patycka <elektropatycka@gmail.com>
2022-07-21 22:17:08 +0200

    Bug fix reading the deniro.csv file using pandas.

--- 04_Reading_files/reading_csv/read_deniro_csv.py
+++ 04_Reading_files/reading_csv/read_deniro_csv.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 deniro_csv = "deniro.csv"
 
-deniro_data = pd.read_csv(deniro_csv, sep=";", decimal=".")
+deniro_data = pd.read_csv(deniro_csv, sep=",", quotechar='"', skipinitialspace=True)
 print(deniro_data)
 
 
